1ec87ff541baf086c42da5bc05e4908d66566603
Léo glme <leo.guillaume@epitech.eu>
2022-04-05 16:19:40 +0200

    signup page

--- pages/signup/script.ts
+++ pages/signup/script.ts
@@ -1,6 +1,7 @@
 import AuthForm from "@/components/Forms/AuthForm/index.vue"
+import Logo from "@/components/Branding/Logo/index.vue"
 export default {
     name: "signup",
-    components: {AuthForm},
+    components: {AuthForm, Logo},
     layout: "anonymous"
 }

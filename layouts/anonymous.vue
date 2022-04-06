bdcdfcf4e96c13cbd50617ac7722682bc384b6f1
Léo glme <leo.guillaume@epitech.eu>
2022-04-06 14:47:32 +0200

    custom loading bar and loading component

--- layouts/anonymous.vue
+++ layouts/anonymous.vue
@@ -1,11 +1,18 @@
 <template>
     <main>
-      <Nuxt/>
+      <Nuxt v-if="$fetchState.pending"/>
+      <div class="center" style="height: 100vh;"  v-if="!$fetchState.pending">
+        <Loading/>
+      </div>
+
     </main>
 </template>
 
 <script lang="ts">
+import Loading from "@/components/Common/Loader/index.vue"
 export default {
-  name: "anonymous"
+  name: "anonymous",
+  components: {Loading},
+  async fetch(){}
 }
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,19 +1,29 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'adopte-un-pet',
+    title: '',
+    titleTemplate: '%s - Adopte un pet',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'fr'
     },
     meta: [
       {charset: 'utf-8'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: ''},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1'},
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Simplifier la gestions des interventions de votre entreprise grâce à Easy Tpe.'
+      },
       {name: 'format-detection', content: 'telephone=no'}
     ],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ]
+  },
+
+  loading: {
+    color: '#8050E2',
+    continuous: true
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -23,7 +33,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~plugins/leaflet.ts', ssr: false }
+    {src: '~plugins/leaflet.ts', ssr: false}
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -33,16 +43,50 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
-    "@nuxt/image"
+    "@nuxt/image",
+    "@nuxtjs/axios",
+    "@nuxtjs/dotenv",
+    "@nuxtjs/proxy",
+    "@nuxtjs/sitemap",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    '@nuxtjs/svg',
     'bootstrap-vue/nuxt',
   ],
 
+  auth: {
+    strategies: {
+      local: {
+        user: {
+          property: ''
+        },
+        token: {
+          property: 'token',
+          name: "Authorization"
+        },
+        endpoints: {
+          login: {url: '/api/account/login', method: 'post', property: 'Token',},
+          logout: {url: '/api/account/logout', method: 'post'},
+          user: {url: '/api/account/user', method: 'post'}
+        }
+      }
+    }
+  },
+
+  proxy: {
+    '/api/': {
+      target: process.env.API_URL || 'http://localhost:3333',
+      pathRewrite: { '^/api/': 'api/' }
+    }
+  },
+  axios: {
+    proxy: true
+  },
+  router: {
+    middleware: ['auth']
+  },
+
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {}
 }

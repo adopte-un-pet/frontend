bb0560d7b2a23f8504cf5fdedfb5aaadc8794d56
Léo glme <leo.guillaume@epitech.eu>
2022-04-08 16:52:03 +0200

    dashboard icon button, navbar, page

---
+++ layouts/dashboard.vue
@@ -0,0 +1,34 @@
+<template>
+
+  <main v-if="!$fetchState.pending">
+    <div>
+      <Navbar/>
+      <div class="dashboard_container">
+        <NavigationMenu/>
+        <Nuxt/>
+      </div>
+
+    </div>
+
+    <section id="loading" class="center" style="height: 100vh;"  v-if="$fetchState.pending">
+      <Loader/>
+    </section>
+  </main>
+</template>
+
+<script lang="ts">
+import Navbar from "@/components/layouts/Navbar/index.vue"
+import NavigationMenu from "@/components/layouts/NavigationMenu/index.vue"
+import Loader from "@/components/Loaders/Loader/index.vue"
+export default {
+  components: {Navbar, Loader, NavigationMenu},
+  async fetch(){}
+}
+</script>
+
+<style>
+  .dashboard_container {
+    min-height: calc(100vh - calc(var(--navbar-height) + 1px));
+    background: #f7f7f8;
+  }
+</style>
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,3 +1,2 @@
 <template src="./index.html"/>
 <script lang="ts" src="./script.ts"/>
-<style lang="scss" scoped src="./style.scss"/>

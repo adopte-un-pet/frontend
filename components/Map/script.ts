b12a311810a4ac84a0d26fa2e752c729532f10a2
Nico <nicolas.de-meyere-dos-santos@epitech.eu>
2022-05-05 15:18:29 +0200

    fix scripts pour ubuntu

--- components/Map/script.ts
+++ components/Map/script.ts
@@ -5,9 +5,9 @@ export default {
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attribution:
         '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      zoom: 15,
-      center: [51.505, -0.159],
-      markerLatLng: [51.504, -0.159]
+      zoom: 30,
+      center: [48.104275, -1.688926],
+      markerLatLng: [48.104275, -1.688926]
     };
   }
 }

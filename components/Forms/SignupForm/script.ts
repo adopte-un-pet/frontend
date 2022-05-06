bbffd5b59a22e237e50d8ba648d6434930164b50
Nico <nicolas.de-meyere-dos-santos@epitech.eu>
2022-05-06 14:59:05 +0200

    fix bug signup=breederSignup

---
+++ components/Forms/SignupForm/script.ts
@@ -0,0 +1,40 @@
+import TextLoad from "@/components/Loaders/TextLoad/index.vue"
+import {ValidationObserver, ValidationProvider} from "vee-validate";
+import Vue from "vue";
+export default Vue.extend({
+    name: "SignupForm",
+    components: {TextLoad, ValidationProvider, ValidationObserver},
+    props: {
+      ButtonText: {type: String, default: "Confirmer"}
+    },
+    data(){
+      return {
+        loading: false,
+        form: {
+          email: "",
+          password: ""
+        }
+      }
+    },
+  computed: {
+    async formIsValid() {
+      const ref = this.$refs.signupObserver as Vue & { validate: () => boolean }
+      return await ref.validate();
+    },
+    formIsEmpty(): boolean {
+      return this.form.email.length === 0 || this.form.password.length === 0;
+    }
+  },
+  methods: {
+    async handleSubmit() {
+      if(await this.formIsValid){
+        this.$emit('submit', this.form)
+      }
+    },
+    formContainErrors(errors: {[key: string]: []}): boolean{
+      if(!errors) return false;
+      if(!errors['Le mot de passe'] || !errors['L\'email']) return false;
+      return errors['Le mot de passe'].length > 0 || errors['L\'email'].length > 0
+    }
+  }
+})
